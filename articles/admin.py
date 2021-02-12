51a529eae668f1a26eec24d42466a416eca417d5
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-02-12 19:58:32 +0300

    Django_for_beginners_vincent_w_Rus_book_DONE_FINISH

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from. import models
-# Register your models here.
-admin.site.register(models.Article)
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
